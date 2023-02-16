5e159ce94a9fe4166c52f40d3b179a7fc3f4f1b9
Oblong9 <98433294+Oblong9@users.noreply.github.com>
2023-02-16 14:47:57 -0500

    Add files via upload

--- SportsDash/Bet.cs
+++ SportsDash/Bet.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.RightsManagement;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SportsDash
 {
-    internal class Bet
+    public class Bet
     {
         private float wager { get; set; }
         private float winnings { get; set; }
@@ -15,5 +16,51 @@ namespace SportsDash
         private String gameWinner { get; set; }
         private Boolean betWin { get; set; } = false;
         private Boolean openBet { get; set; } = true;
+
+        public Bet(float wager, int odds, float winnings)
+        {
+            this.wager = wager;
+            this.odds = odds;
+            this.winnings = winnings;
+        }
+        
+        public Bet(float wager, int odds, float winnings, String game, String gameWinner, Boolean betWin, Boolean openBet) 
+        {
+            this.wager = wager;
+            this.odds = odds;
+            this.winnings = winnings;
+            this.game = game;
+            this.gameWinner = gameWinner;
+            this.betWin = betWin;
+            this.openBet = openBet;
+            this.betWin = openBet;
+        }
+
+        // Function for calculating the amount won including the wager
+        public float calculateWins(int odds)
+        {
+            int multiplier;
+
+            if(odds < 0)
+            {
+                multiplier = 100 / odds * -1;
+                winnings = wager * multiplier + wager;
+
+            }
+            else
+            {
+                multiplier = odds / 100;
+                winnings = wager * multiplier + wager;
+            }
+            return winnings;
+        }
+
+        public String ToString()
+        {
+            return "Bet:\nGame: " + game + " | Game Winner: " + gameWinner + " | Wager: " + wager + " | Odds: " + odds +
+                " | Bet Win: " + betWin + " | Open Bet: " + openBet;
+        }
+
+
     }
 }
